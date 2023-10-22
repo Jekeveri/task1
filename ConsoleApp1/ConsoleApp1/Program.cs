41b6891da71a33654cdbc8c7779cabd789beca9f
Jekeveri <pickjeik@gmail.com>
2023-10-22 21:40:56 +0500

    Add existing file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //мейн не скопировал когда сохранял
+        }
+
+        public class MyCollection<T>
+        {
+            private ArrayList arrayListItems = new ArrayList();
+            private List<T> listItems = new List<T>();
+            private Stack<T> stackItems = new Stack<T>();
+            private Queue<T> queueItems = new Queue<T>();
+
+            public void AddArrayList(object item)
+            {
+                arrayListItems.Add(item);
+            }
+
+            public void RemoveArrayList(object item)
+            {
+                arrayListItems.Remove(item);
+            }
+
+            public object GetArrayList(int index)
+            {
+                return arrayListItems[index];
+            }
+
+            public int CountArrayList()
+            {
+                return arrayListItems.Count;
+            }
+
+
+
+
+            public void AddList(T item)
+            {
+                listItems.Add(item);
+            }
+
+            public void RemoveList(T item)
+            {
+                listItems.Remove(item);
+            }
+
+            public T GetList(int index)
+            {
+                return listItems[index];
+            }
+
+            public int CountList()
+            {
+                return listItems.Count;
+            }
+
+
+
+
+
+            public void PushStack(T item)
+            {
+                stackItems.Push(item);
+            }
+
+            public T PopStack()
+            {
+                return stackItems.Pop();
+            }
+
+            public T PeekStack()
+            {
+                return stackItems.Peek();
+            }
+
+            public int CountStack()
+            {
+                return stackItems.Count;
+            }
+
+
+
+
+            public void EnqueueQueue(T item)
+            {
+                queueItems.Enqueue(item);
+            }
+
+            public T DequeueQueue()
+            {
+                return queueItems.Dequeue();
+            }
+
+            public T PeekQueue()
+            {
+                return queueItems.Peek();
+            }
+
+            public int CountQueue()
+            {
+                return queueItems.Count;
+            }
+        }
+
+    }
+}
---
+++ Slovar/Slovar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slovar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> placesAndTeams = new Dictionary<string, string>();
+            placesAndTeams.Add("Москва", "ЦСКА");
+            placesAndTeams.Add("Санкт-Петербург", "Зенит");
+            placesAndTeams.Add("Казань", "Рубин");
+            placesAndTeams.Add("Екатеринбург", "Урал");
+            placesAndTeams.Add("Краснодар", "Краснодар");
+            placesAndTeams.Add("Ростов-на-Дону", "Ростов");
+            placesAndTeams.Add("Нижний Новгород", "Олимпиец");
+            placesAndTeams.Add("Самара", "Крылья Советов");
+            placesAndTeams.Add("Волгоград", "Ротор");
+            placesAndTeams.Add("Уфа", "Уфа");
+
+            Console.Write("Введите название города: ");
+            string city = Console.ReadLine();
+
+            if (placesAndTeams.ContainsKey(city))
+            {
+                Console.WriteLine($"Команда из города {city}: {placesAndTeams[city]}");
+
+                Console.Write("Этот элемент будет удален словаря ");
+                placesAndTeams.Remove(city);
+
+                Console.WriteLine("Оставшиеся элементы словаря:");
+                foreach (KeyValuePair<string, string> i in placesAndTeams)
+                {
+                    Console.WriteLine($"{i.Key}: {i.Value}");
+                }
+
+                Console.Write("Удаление всех элементов словаря ");
+                placesAndTeams.Clear();
+            }
+            else
+            {
+                Console.WriteLine($"В словаре нет информации о городе {city}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
